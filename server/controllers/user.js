7e158b9cc2e629a83ed47e396d71c6ca5bfe4bd1
Yong <ycho97@uwo.ca>
2020-11-12 21:21:02 -0500

    redis store/register backend done

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,3 +1,32 @@
+const User = require('../models/user');
+
 exports.login = (req, res) => {
     
+}
+
+exports.register = async (req, res) => {
+    const {username, password, confirmPassword} = req.body;
+
+    const user = await User.findOne({username});
+
+    if(password !== confirmPassword){
+        res.json({msg: 'Passwords do not match'});
+    }
+
+    else if(user){
+        res.json({msg: 'Username already exists'});
+    }
+
+    else{
+        const newUser = new User({
+            username,
+            password
+        });
+
+        const user = await newUser.save();
+
+        req.session.uid = user._id;
+        
+        res.json({msg: 'Success'});
+    }
 }
--- server/models/user.js
+++ server/models/user.js
@@ -2,8 +2,30 @@ const bcrypt = require('bcrypt');
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-
+    username: String,
+    password: String
 });
 
+UserSchema.pre('save', async function(next){
+    const salt = await bcrypt.genSalt(); 
+    this.password = await bcrypt.hash(this.password, salt); 
+    next();
+});
+
+UserSchema.static.login = async function(username, password){
+    const user = await this.findOne({username});
+ 
+    if(user){
+        const auth = await bcrypt.compare(password, this.password);
+
+        const success = {msg: 'Success', uid: user._id};
+        const failure = {msg: 'Incorrect password'};
+
+        return auth? success: failure;
+    } else{
+        return {msg: 'User is not registered'};
+    }
+}
+
 const User = mongoose.model('user', UserSchema);
-exports.User = User;
+module.exports = User;
