ecbffded48f84d78236d4c86010bebef3d9b4041
Tenzin Dorjee <tenzindorjee@Tenzins-iMac.local>
2020-11-18 21:44:56 -0700

    DeadPage

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
+import DeadPage from "./components/layout/DeadPage";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Switch>
           <Route exact path='/login' component={Login}/>
           <Route exact path='/register' component={Register}/>
+          <Route path='/' component={DeadPage}/>
+
         </Switch>
       </BrowserRouter>
       
