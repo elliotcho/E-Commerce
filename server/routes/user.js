2e64f039cb3ab652e861ce6b360f22e640384990
Tenzin Dorjee <tenzindorjee@Tenzins-iMac.local>
2020-11-19 21:09:38 -0700

    Change Username Route

--- server/routes/user.js
+++ server/routes/user.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { login,  register } from '../controllers/user';
+import { login,  register, change_username } from '../controllers/user';
 
 const router = express.Router();
 
 router.post('/login', login);
 router.post('/register', register);
-
+router.post('/change_username', change_username);
 export default router;
