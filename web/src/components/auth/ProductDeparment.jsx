c824d413c1b2190aea91c0d466e5ba12914ba6a7
Charan Gautam <gautamsaicharan@yahoo.com>
2020-11-23 21:20:18 -0700

    attempt

---
+++ web/src/components/auth/ProductDeparment.jsx
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+class ProductDepartment extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            sameDepartment: null
+        }
+    }
+
+    async componentDidMount(){
+        const response = await axios.get('http://localhost:5000/api/product/department/departmentId');
+        const department = response.data;
+  
+        this.setState({sameDepartment:department});
+        
+
+
+    }
+
+    render(){
+            const {sameDepartment} = this.state;
+            console.log(sameDepartment);
+        return(
+            <div>
+            
+            </div>
+        )
+    }
+}
+
+export default ProductDepartment;
