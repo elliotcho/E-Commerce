ea0ffcb8ff108b4e7c1093632faac8d83ee491f6
Armaan Mohar <armaan.mohar@gmail.com>
2020-11-23 21:08:42 -0700

    delete account bug

--- server/routes/user.js
+++ server/routes/user.js
@@ -5,7 +5,8 @@ import {
     logout, 
     change_username,
     forgotPassword, 
-    changePassword 
+    changePassword,
+    deleteAccount 
 } from '../controllers/user';
 
 const router = express.Router();
@@ -16,5 +17,6 @@ router.post('/change_username', change_username);
 router.get('/logout', logout);
 router.post('/forgot_password', forgotPassword);
 router.post('/change_password', changePassword);
+router.delete('/deleteAccount', deleteAccount);
 
 export default router;
