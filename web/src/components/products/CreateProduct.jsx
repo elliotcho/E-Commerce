2756d3753c082f555a75ed1c87f138d0a9812450
Charan Gautam <gautamsaicharan@yahoo.com>
2020-11-25 20:43:51 -0700

    hella backend

---
+++ web/src/components/products/CreateProduct.jsx
@@ -0,0 +1,80 @@
+import React, {Component} from 'react';
+import axios from 'axios';
+
+import './css/CreateProduct.css';
+
+class CreateProduct extends Component {
+    constructor(){
+        super();
+
+        this.state = {
+            price: null,
+            productName: '',
+            departmentId: ''
+        }
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({[e.target.name]: e.target.value});
+    }
+
+    async handleSubmit(e){
+        e.preventDefault();
+
+        const {price, productName, departmentId} = this.state;
+
+        const response = await axios.post('http://localhost:5000/api/product', this.state);
+
+
+    }
+
+    // async componentDidMount(){
+    //     const response = await axios.post('http://localhost:5000/api/product');
+    //     const newProduct = response.data;
+
+    //     this.setState({newProduct:newProduct});
+    // }
+
+
+    render(){
+        const {price, productName, departmentId} = this.state;
+
+        return(
+            <div>
+                <form className='createProduct' onSubmit={this.handleSubmit}>
+                    <h1>List A Product</h1>
+                    <label htmlFor="productName">Product Name</label>
+                    <input 
+                        name='productName'
+                        value={productName}
+                        onChange={this.handleChange}
+                        type='text'
+                        required
+                    />
+                    <label htmlFor="departmentId">Department ID</label>
+                    <input 
+                        name='departmentId'
+                        value={departmentId}
+                        onChange={this.handleChange}
+                        type="text"
+                        required
+                    />
+                    <label htmlFor="price">Price</label>
+                    <input 
+                        name='price'
+                        value={price}
+                        onChange={this.handleChange} 
+                        type="number" 
+                        placeholder='$'
+                    />
+                    <button>CREATE</button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default CreateProduct;
