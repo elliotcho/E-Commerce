6c5f5316bf7789605be5c856f5c00351bb663b45
Charan Gautam <gautamsaicharan@yahoo.com>
2020-12-23 20:58:52 -0700

    start of messageBoard

---
+++ web/src/components/messages/MessageHome.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import './css/MessageHome.css';
+
+
+class MessageHome extends Component{
+    constructor(){
+        super();
+    }
+
+    render(){
+        return(
+            <div className='messageHome'>
+                <h1>Message Home</h1> 
+                <section className='container-fluid row'>
+                    <div className='userBoard col-5'>
+                        <i class="fas fa-users"></i>
+                    </div>
+                    <div className='messageBoard col-7'>
+                        <form>
+                            <input type='text'/>
+                            
+                        </form>
+                    </div>
+                </section>
+            </div>
+        )
+    }
+}
+
+export default MessageHome;
