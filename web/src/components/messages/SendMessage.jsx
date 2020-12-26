38ea530ba3c5809f6c5faf89a69598ebc3289508
Armaan Mohar <armaan.mohar@gmail.com>
2020-12-26 16:31:14 -0700

    create msg connected to backend

--- web/src/components/messages/SendMessage.jsx
+++ web/src/components/messages/SendMessage.jsx
@@ -1,16 +1,38 @@
 import React from 'react';
+import {sendMessage} from '../../api/message'
 import './css/SendMessage.css';
 
 const ENTER_KEY = 13;
 
 class SendMessage extends React.Component{
-    handleKeyDown(e){
+    constructor(){
+        super();
+        this.state = {
+            content: ''
+        }
+        this.handleChange = this.handleChange.bind(this);
+        this.handleKeyDown = this.handleKeyDown.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({[e.target.id]: e.target.value});
+    }
+
+    async handleKeyDown(e){
         if(e.keyCode === ENTER_KEY){
-            
+            const receiverID = this.props.userId;
+            const content = this.state;
+            const data = {
+                receiverID,
+                content
+            }
+            const response = await sendMessage(data);
+            console.log(response);
         }
     }
 
     render(){
+        const content = this.state;
         return(
             <div className='send-message'>
                 <button>
@@ -21,6 +43,9 @@ class SendMessage extends React.Component{
                     type = 'text'
                     placeholder = 'Your Message Here'
                     onKeyDown = {this.handleKeyDown}
+                    onChange = {this.handleChange}
+                    value = {content}
+                    id= 'content'
                 />
             </div>
         )
