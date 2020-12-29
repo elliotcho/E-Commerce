d45c55008d17492cc20591508a0f75cf31252ca1
Yong <ycho97@uwo.ca>
2020-12-28 22:51:16 -0500

    using redis to save socket connections

--- server/socket/index.js
+++ server/socket/index.js
@@ -1,13 +1,22 @@
 import { redis } from '../app';
+import jwt from 'jsonwebtoken';
 
 const socketEvents = (io) => {
     io.on('connection', socket => {
-        socket.on('JOIN', data => {
-            console.log(data);
+        socket.on('JOIN', async data => {
+            const { token } = data;
+
+            const { user: { _id } } = jwt.decode(token);
+        
+            await redis.set(_id, socket.id);
         });
 
-        socket.on('DISCONNECT', data => {
-            console.log(data);
+        socket.on('DISCONNECT', async data => {
+            const { token } = data;
+
+            const { user: { _id } } = jwt.decode(token);
+
+            await redis.del(_id);
         })
     });
 }
