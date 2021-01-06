0b78e7d1aff0264fa015db6663821e8ab8601bec
Armaan Mohar <armaan.mohar@gmail.com>
2021-01-05 21:16:57 -0700

    tiger

--- web/src/components/home/Home.jsx
+++ web/src/components/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import tiger from "../../images/tiger.jpg";
 import './css/Home.css';
 
 function Home(){
     return(
         <div className = 'home'>
-            HELLO WORLD
+            <div id="bg">
+                 <img src={tiger} alt=''/>
+            </div>
         </div>
     )
 }
--- web/src/components/settings/Settings.jsx
+++ web/src/components/settings/Settings.jsx
@@ -3,14 +3,53 @@ import { ThemeContext } from '../../contexts/ThemeContext';
 import DeleteUser from './DeleteUser.jsx';
 import UsernameForm from './UsernameForm';
 import ChangePasswordForm from './ChangePasswordForm';
+import {DarkModeSwitch} from 'react-toggle-dark-mode';
 import './css/Setting.css';
+
 
 const lightStyle = { backgroundColor: '#9ad3bc', color: '#3f3e3e' };
 const darkStyle = { backgroundColor: '#34626c', color: 'white' };
+const defaultProperties = {
+    dark: {
+      circle: {
+        r: 9,
+      },
+      mask: {
+        cx: '50%',
+        cy: '23%',
+      },
+      svg: {
+        transform: 'rotate(40deg)',
+      },
+      lines: {
+        opacity: 0,
+      },
+    },
+    light: {
+      circle: {
+        r: 5,
+      },
+      mask: {
+        cx: '100%',
+        cy: 0,
+      },
+      svg: {
+        transform: 'rotate(90deg)',
+      },
+      lines: {
+        opacity: 1,
+      },
+    },
+    springConfig: { mass: 4, tension: 250, friction: 35 },
+  };
 
 function Settings(){
     const { isDark, toggleTheme } = useContext(ThemeContext);
-
+    let checked = false;
+    const{isDarkMode, setDarkMode} = React.useState(false);
+    const toggleDarkMode = (checked) =>{
+        setDarkMode(checked);
+    };
     const style = (isDark) ? darkStyle: lightStyle;
 
     return(
@@ -21,6 +60,13 @@ function Settings(){
                 </div>
                 
                 <div className='col-3'>
+                   <DarkModeSwitch
+                    style={{marginBottom: '2rem'}}
+                    checked= {isDarkMode}
+                    onChange={toggleDarkMode}
+                    size={120}
+                    animationProperties={defaultProperties}
+                    />
                     <button onClick={toggleTheme}>
                         {isDark? 'Light mode' : 'night mode'}
                     </button>
